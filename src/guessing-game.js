e41dc4b1065e63f2958fb62dd28c9eaa645797de
BrBrov <epoc@list.ru>
2022-08-15 14:25:54 +0300

    feat: Create guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;        
     }
 
     guess() {
-
+       this.num = Math.ceil((this.max-this.min)/2 + this.min);
+       return  this.num;
     }
 
     lower() {
-
+        this.max = this.num;
     }
 
     greater() {
-
+        this.min = this.num;
     }
 }
 
